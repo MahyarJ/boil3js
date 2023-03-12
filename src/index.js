49cc8c2447720327d5b9d8c5341f3cbbaaaa4cb7
Mahyar Yahyajamalabadi <mahyar.jamalabadi@gmail.com>
2023-03-12 20:13:02 +0200

    Add WebGL availability check

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import WebGL from "three/examples/jsm/capabilities/WebGL";
 
 import animate from "./animate";
 import init from "./init";
 
-const { scene, camera, renderer } = init();
-document.body.appendChild(renderer.domElement);
+if (WebGL.isWebGLAvailable()) {
+  // Initiate function or other initializations here
+  const { scene, camera, renderer } = init();
+  document.body.appendChild(renderer.domElement);
 
-// To check import jsm
-const controls = new OrbitControls(camera, renderer.domElement);
-console.log(controls);
+  // To check import jsm
+  const controls = new OrbitControls(camera, renderer.domElement);
+  console.log(controls);
 
-// Add our cube
-const geometry = new THREE.BoxGeometry(1, 1, 4);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xdddddd,
-  wireframe: true,
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+  // Add our cube
+  const geometry = new THREE.BoxGeometry(1, 1, 4);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xdddddd,
+    wireframe: true,
+  });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 
-animate({ scene, camera, renderer, cube });
+  animate({ scene, camera, renderer, cube });
+} else {
+  const warning = WebGL.getWebGLErrorMessage();
+  document.body.appendChild(warning);
+}
